ec503bfd6bf54978ce1f3ed5024191f34b1b714c
IrriionDeveloper <168735493+IrriionDeveloper@users.noreply.github.com>
2024-12-04 17:06:27 +0530

    Daybook changes

--- database/migrations/2024_11_25_104903_create_get_daybook_report_procedure.php
+++ database/migrations/2024_11_25_104903_create_get_daybook_report_procedure.php
@@ -34,36 +34,58 @@ return new class extends Migration
 
                 SELECT
                     v.voucher_date,
-                    GROUP_CONCAT(l.ledger_name SEPARATOR ', ') AS `ledger_name`,
+                    COALESCE(pl.ledger_name, al.ledger_name) AS `ledger_name`,
                     c.company_name,
                     vt.voucher_type_name,
                     v.voucher_number,
-                    SUM(
-                        CASE 
-                            WHEN vh.entry_type = 'debit' THEN ABS(vh.amount) 
-                            ELSE 0 
-                        END
-                    ) AS `total_debit`,
-                    SUM(
-                        CASE 
-                            WHEN vh.entry_type = 'credit' THEN vh.amount 
-                            ELSE 0 
-                        END
-                    ) AS `total_credit`
+                    CASE
+                        WHEN COALESCE(pl.entry_type, al.entry_type) = 'debit' THEN ABS(COALESCE(pl.amount, al.amount))
+                        ELSE 0
+                    END AS `total_debit`,
+                    CASE
+                        WHEN COALESCE(pl.entry_type, al.entry_type) = 'credit' THEN ABS(COALESCE(pl.amount, al.amount))
+                        ELSE 0
+                    END AS `total_credit`
                 FROM
                     tally_vouchers v
                 JOIN
                     tally_companies c ON v.company_id = c.company_id
                 JOIN
                     tally_voucher_types vt ON v.voucher_type_id = vt.voucher_type_id
-                JOIN
-                    tally_voucher_heads vh ON v.voucher_id = vh.voucher_id
-                JOIN
-                    tally_ledgers l ON vh.ledger_id = l.ledger_id
+                LEFT JOIN (
+                        SELECT
+                            vh.voucher_id,
+                            MIN(l.ledger_name) AS ledger_name,
+                            MIN(vh.amount) AS amount,
+                            MIN(vh.entry_type) AS entry_type
+                        FROM
+                            tally_voucher_heads vh
+                        JOIN
+                            tally_ledgers l ON vh.ledger_id = l.ledger_id
+                        WHERE
+                            vh.is_party_ledger = 1
+                        GROUP BY
+                            vh.voucher_id
+                    ) pl ON v.voucher_id = pl.voucher_id
+
+                LEFT JOIN (
+                        SELECT
+                            vh.voucher_id,
+                            MIN(l.ledger_name) AS ledger_name,
+                            MIN(vh.amount) AS amount,
+                            MIN(vh.entry_type) AS entry_type
+                        FROM
+                            tally_voucher_heads vh
+                        JOIN
+                            tally_ledgers l ON vh.ledger_id = l.ledger_id
+                        GROUP BY
+                            vh.voucher_id
+                    ) al ON v.voucher_id = al.voucher_id AND pl.voucher_id IS NULL
+
                 WHERE
                     v.is_optional = p_is_optional
                     AND v.is_cancelled = p_is_cancelled
-                    AND FIND_IN_SET(l.company_id, p_company_ids)
+                    AND FIND_IN_SET(v.company_id, p_company_ids)
                     AND (p_start_date IS NULL OR v.voucher_date >= p_start_date)
                     AND (p_end_date IS NULL OR v.voucher_date <= p_end_date)
                     AND (
